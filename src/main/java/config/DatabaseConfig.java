a4206d3c6444cdd960a7d70b03c032b851bfeafb
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2025-03-30 16:06:15 +0400

    DatabaseConfig

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -5,11 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    private static final String URL = System.getenv("DB_URL");
-    private static final String USER = System.getenv("DB_USER");
-    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+    private static final String URL = "jdbc:postgresql://localhost:5432/hotel_booking";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
